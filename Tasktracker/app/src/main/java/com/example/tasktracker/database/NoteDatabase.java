ccd69519a894681da6ac893b060fd43cca91a0ad
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-12 23:14:24 +0530

    Added NoteDatabase abstract class

--- Tasktracker/app/src/main/java/com/example/tasktracker/database/NoteDatabase.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/database/NoteDatabase.java
@@ -1,2 +1,27 @@
-package com.example.tasktracker.database;public class NoteDatabase {
+package com.example.tasktracker.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.tasktracker.DAO.NoteDAO;
+import com.example.tasktracker.entities.Note;
+
+@Database(entities = {Note.class}, version = 1, exportSchema = false)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    public abstract NoteDAO noteDAO();
+    private static NoteDatabase instance;
+
+    public static synchronized NoteDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 }
