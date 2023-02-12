fbedab328be4b8afaec802655b25ec482db8a2aa
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-12 23:17:59 +0530

    Added AddEditActivityViewModel class

--- Tasktracker/app/src/main/java/com/example/tasktracker/viewModel/AddEditActivityViewModel.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/viewModel/AddEditActivityViewModel.java
@@ -1,2 +1,43 @@
-package com.example.tasktracker.viewModel;public class AddEditActivityViewModel {
+package com.example.tasktracker.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.tasktracker.entities.Note;
+import com.example.tasktracker.repository.NoteRepository;
+
+public class AddEditActivityViewModel extends AndroidViewModel {
+
+    private NoteRepository noteRepository;
+    private MutableLiveData<Note> noteLiveData = new MutableLiveData<>();
+
+    public AddEditActivityViewModel(@NonNull Application application) {
+        super(application);
+        noteRepository = new NoteRepository(application);
+    }
+
+    public void insertNote(Note note){
+        noteRepository.insert(note);
+    }
+
+    public void updateNote(Note note){
+        noteRepository.update(note);
+    }
+
+    public void deleteNote(Note note){
+        noteRepository.delete(note);
+    }
+
+    public void setNote(Note note){
+        noteLiveData.setValue(note);
+    }
+
+    public MutableLiveData<Note> getNote(){
+        return noteLiveData;
+    }
+
+
 }
