c5b9c51ee83a648ae34fc0b7f11e7284bb4b6236
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-12 23:20:13 +0530

    Added new drawable files

--- Tasktracker/app/src/main/java/com/example/tasktracker/AddEditActivity.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/AddEditActivity.java
@@ -1,14 +1,46 @@
 package com.example.tasktracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.example.tasktracker.entities.Note;
+import com.example.tasktracker.viewModel.AddEditActivityViewModel;
+
 public class AddEditActivity extends AppCompatActivity {
+
+    private Context context;
+    private AddEditActivityViewModel addEditActivityViewModel;
+    private ViewDataBinding activityAddEditBinding;
+    private boolean isEditMode;
+    private Note currentNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit);
+
+        addEditActivityViewModel = new AddEditActivityViewModel(getApplication());
+        activityAddEditBinding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.activity_add_edit, null, false);
+        activityAddEditBinding.setVariable(BR.viewModel, addEditActivityViewModel);
+        activityAddEditBinding.setLifecycleOwner(this);
+
+        context = getBaseContext();
+        isEditMode = getIntent().getBooleanExtra(MainActivity.IS_EDIT_MODE,false);
+
+
+        if (!isEditMode){
+            currentNote = new Note();
+            addEditActivityViewModel.setNote(currentNote);
+            
+
+
+        }
+
+
+
     }
 }
--- Tasktracker/app/src/main/java/com/example/tasktracker/MainActivity.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.tasktracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.tasktracker.databinding.ActivityMainBinding;
+import com.example.tasktracker.viewModel.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MainActivityViewModel mainActivityViewModel;
+    private ActivityMainBinding activityMainBinding;
+    private MainHandler mainHandler;
+
+    public static final String IS_EDIT_MODE = "isEditMode";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_edit);
+
+
+        mainActivityViewModel = new MainActivityViewModel(getApplication());
+        activityMainBinding = DataBindingUtil.inflate(getLayoutInflater(),R.layout.activity_main,null,false);
+        mainHandler = new MainHandler();
+
+    }
+
+    private class MainHandler{
+
+        public void onAddNoteClick(View view){
+            Intent intent = new Intent(MainActivity.this,AddEditActivity.class);
+            intent.putExtra(IS_EDIT_MODE,false);
+            startActivity(intent);
+        }
+
+
     }
 }
