9266917cff4c019f207c9a2555127d6f6ae69d1d
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-12 23:18:39 +0530

    Added Main Activity view model class

--- Tasktracker/app/src/main/java/com/example/tasktracker/viewModel/MainActivityViewModel.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/viewModel/MainActivityViewModel.java
@@ -1,2 +1,53 @@
-package com.example.tasktracker.viewModel;public class MainActivityViewModel {
+package com.example.tasktracker.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.tasktracker.entities.Note;
+import com.example.tasktracker.repository.NoteRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private NoteRepository noteRepository;
+    private LiveData<List<Note>> allNotes;
+    private LiveData<List<Note>> allPinnedNotes;
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        noteRepository = new NoteRepository(application);
+        allNotes = noteRepository.getAllNotes();
+        allPinnedNotes = noteRepository.getAllPinnedNotes();
+    }
+
+    public void insert(Note note){
+        noteRepository.insert(note);
+    }
+
+    public void update(Note note){
+        noteRepository.update(note);
+    }
+
+    public void delete(Note note){
+        noteRepository.delete(note);
+    }
+
+    public void deleteAllNotes(){
+        noteRepository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes(){
+        return allNotes;
+    }
+
+    public LiveData<List<Note>> getAllPinnedNotes(){
+        return allPinnedNotes;
+    }
+
+    public LiveData<List<Note>> getAllNotesAccordingToSearchQuery(String searchQuery){
+        return noteRepository.getAllNotesAccordingToSearchQuery(searchQuery);
+    }
 }
