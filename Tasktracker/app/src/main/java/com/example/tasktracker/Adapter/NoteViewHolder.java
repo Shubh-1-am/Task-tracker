cd26a3d6735dc825ed17e28a48ac178074e13159
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-14 21:45:29 +0530

    Added NoteViewHolder for Adapter class

--- Tasktracker/app/src/main/java/com/example/tasktracker/Adapter/NoteViewHolder.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/Adapter/NoteViewHolder.java
@@ -1,2 +1,93 @@
-package com.example.tasktracker.Adapter;public class NoteViewHolder {
+package com.example.tasktracker.Adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tasktracker.databinding.NoteItemBinding;
+import com.example.tasktracker.entities.Note;
+import com.google.android.material.textview.MaterialTextView;
+
+import io.noties.markwon.AbstractMarkwonPlugin;
+import io.noties.markwon.Markwon;
+import io.noties.markwon.MarkwonVisitor;
+import io.noties.markwon.core.CorePlugin;
+import io.noties.markwon.ext.strikethrough.StrikethroughPlugin;
+import io.noties.markwon.ext.tasklist.TaskListPlugin;
+
+public class NoteViewHolder extends RecyclerView.ViewHolder {
+
+    private NoteItemBinding noteItemBinding;
+    private Note currentNote;
+
+    public NoteViewHolder(@NonNull NoteItemBinding noteItemBinding) {
+        super(noteItemBinding.getRoot());
+        this.noteItemBinding = noteItemBinding;
+    }
+
+    public void setNoteBackground(){
+        LinearLayout noteLayout = noteItemBinding.noteItemParentLinearlayout;
+        noteLayout.setBackgroundResource(currentNote.getNoteBackground());
+    }
+    public void setPinImage() {
+        noteItemBinding.noteItemNoteImagePinImageview.setVisibility(currentNote.isPinned() ? View.VISIBLE : View.GONE);
+    }
+
+    public void setNoteImage() {
+        // Display image from absoute path current.noteImage
+        noteItemBinding.noteItemNoteImageImageview.setVisibility(currentNote.getNoteImage() != null ? View.VISIBLE : View.GONE);
+        noteItemBinding.noteItemNoteImageImageview.setImageURI(Uri.parse(currentNote.getNoteImage()));
+
+    }
+
+    public void setNote(Note note) {
+        noteItemBinding.setNote(note);
+        currentNote = note;
+    }
+
+    public void setNoteUrl(Context context) {
+        if (currentNote.getMarkdownLinkText() != null) {
+            MaterialTextView urlTextView = noteItemBinding.noteItemNoteUrlTextview;
+            Markwon markwon = Markwon.builder(context)
+                    .usePlugin(StrikethroughPlugin.create())
+                    .usePlugin(TaskListPlugin.create(context))
+                    .usePlugin(CorePlugin.create())
+                    .usePlugin(new AbstractMarkwonPlugin() {
+                        @Override
+                        public void configureVisitor(@NonNull MarkwonVisitor.Builder builder) {
+                            super.configureVisitor(builder);
+                        }
+                    }).build();
+            markwon.setMarkdown(urlTextView, currentNote.getMarkdownLinkText());
+            urlTextView.setMovementMethod(LinkMovementMethod.getInstance());
+            noteItemBinding.noteItemNoteUrlLinearlayout.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    public void setNoteDescription(Context context) {
+        if (currentNote.getDescription() != null){
+            MaterialTextView descriptionTextView = noteItemBinding.noteItemNoteDescriptionTextview;
+
+            Markwon markwon = Markwon.builder(context)
+                    .usePlugin(StrikethroughPlugin.create())
+                    .usePlugin(TaskListPlugin.create(context))
+                    .usePlugin(CorePlugin.create())
+                    .usePlugin(new AbstractMarkwonPlugin() {
+                        @Override
+                        public void configureVisitor(@NonNull MarkwonVisitor.Builder builder) {
+                            super.configureVisitor(builder);
+                        }
+                    }).build();
+            markwon.setMarkdown(descriptionTextView, currentNote.getDescription());
+            noteItemBinding.noteItemNoteDescriptionTextview.setVisibility(View.VISIBLE);
+        }
+
+
+    }
 }
