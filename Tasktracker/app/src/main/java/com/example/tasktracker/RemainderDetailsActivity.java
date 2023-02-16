ecbdcb4f00a43de11c91dabfaf6db679814ecfad
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-16 21:20:43 +0530

    Added remainder detail activity

---
+++ Tasktracker/app/src/main/java/com/example/tasktracker/RemainderDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.tasktracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RemainderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remainder_details);
+    }
+}
