2944a0c6d7c10eac5409226755a99400d8b9c3bc
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-17 20:53:03 +0530

    Created RemainderDao interface

--- Tasktracker/app/src/main/java/com/example/tasktracker/DAO/RemainderDAO.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/DAO/RemainderDAO.java
@@ -1,2 +1,37 @@
-package com.example.tasktracker.DAO;public interface RemainderDAO {
+package com.example.tasktracker.DAO;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.tasktracker.entities.Remainder;
+
+import java.util.List;
+
+@Dao
+public interface RemainderDAO {
+
+    @Insert
+    void insert(Remainder remainder);
+
+    @Update
+    void update(Remainder remainder);
+
+    @Delete
+    void delete(Remainder remainder);
+
+    @Query("DELETE FROM remainder_table")
+    void deleteAllRemainders();
+
+    @Query("SELECT * FROM remainder_table ORDER BY DATE ASC, TIME ASC")
+    LiveData<List<Remainder>> getAllRemainders();
+
+    @Query("SELECT * FROM remainder_table WHERE title LIKE :searchQuery OR date LIKE :searchQuery OR time LIKE :searchQuery ORDER BY DATE ASC, TIME ASC")
+    LiveData<List<Remainder>> getAllRemaindersAccordingToSearchQuery(String searchQuery);
+
+
+
 }
