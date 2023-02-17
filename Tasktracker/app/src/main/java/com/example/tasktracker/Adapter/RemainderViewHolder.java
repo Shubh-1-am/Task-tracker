239e1885bac5fbdcc5d3e6a468c91542193dff92
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-17 20:53:45 +0530

    created viewholder for Remainder

--- Tasktracker/app/src/main/java/com/example/tasktracker/Adapter/RemainderViewHolder.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/Adapter/RemainderViewHolder.java
@@ -1,2 +1,41 @@
-package com.example.tasktracker.Adapter;public class RemainderViewHolder {
+package com.example.tasktracker.Adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tasktracker.databinding.RemainderItemBinding;
+import com.example.tasktracker.entities.Remainder;
+
+public class RemainderViewHolder extends RecyclerView.ViewHolder{
+
+    private final RemainderItemBinding remainderItemBinding;
+    private Remainder currentRemainder;
+
+    public RemainderViewHolder(@NonNull RemainderItemBinding remainderItemBinding) {
+        super(remainderItemBinding.getRoot());
+        this.remainderItemBinding = remainderItemBinding;
+
+        remainderItemBinding.remainderItemDeleteImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        remainderItemBinding.remainderIetmEditImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+    public void setRemainder(Remainder remainder){
+        this.currentRemainder = remainder;
+        remainderItemBinding.setRemainder(remainder);
+    }
 }
