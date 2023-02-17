b4d8960ff115451467a2ea782b06d43f9f297898
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-17 20:51:22 +0530

    Created remainder entity class

--- Tasktracker/app/src/main/java/com/example/tasktracker/entities/Remainder.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/entities/Remainder.java
@@ -1,2 +1,91 @@
-package com.example.tasktracker.entities;public class Remainder {
+package com.example.tasktracker.entities;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import com.example.tasktracker.BR;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity(tableName = "remainder_table")
+public class Remainder extends BaseObservable implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "title")
+    private String title;
+    @ColumnInfo(name = "date")
+    private String date;
+    @ColumnInfo(name = "time")
+    private String time;
+
+    public Remainder(int id, String title, String date, String time) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+
+    @Ignore
+    public Remainder() {
+
+    }
+
+    @Bindable
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+        notifyPropertyChanged(BR.id);
+    }
+
+    @Bindable
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        notifyPropertyChanged(BR.title);
+    }
+
+    @Bindable
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+        notifyPropertyChanged(BR.date);
+    }
+
+    @Bindable
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+        notifyPropertyChanged(BR.time);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Remainder remainder = (Remainder) o;
+        return id == remainder.id && title.equals(remainder.title) && date.equals(remainder.date) && time.equals(remainder.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, date, time);
+    }
 }
