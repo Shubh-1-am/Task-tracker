bd8f4375df3bb6f444d62d42274d64e24457db87
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-17 20:52:28 +0530

    Added AlarmBroadcast class

--- Tasktracker/app/src/main/java/com/example/tasktracker/broadcastReceiver/AlarmBroadcast.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/broadcastReceiver/AlarmBroadcast.java
@@ -1,4 +1,17 @@
 package com.example.tasktracker.broadcastReceiver;
 
-public class AlarmBroadcast {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AlarmBroadcast extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        String title = bundle.getString("title");
+        String date_time = bundle.getString("date");
+    }
 }
