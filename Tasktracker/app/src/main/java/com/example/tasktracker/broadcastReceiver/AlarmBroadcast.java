3138d6cfd9614e7d3813e9c610c744aa4b10a43f
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-20 21:08:55 +0530

    Updated AlarmBroadcast class

--- Tasktracker/app/src/main/java/com/example/tasktracker/broadcastReceiver/AlarmBroadcast.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/broadcastReceiver/AlarmBroadcast.java
@@ -25,6 +25,7 @@ import androidx.core.content.ContextCompat;
 import com.example.tasktracker.OnNotifyRemainderListener;
 import com.example.tasktracker.R;
 import com.example.tasktracker.RemainderDetailsActivity;
+import com.example.tasktracker.UtilApplication;
 import com.example.tasktracker.entities.Remainder;
 
 import java.io.Serializable;
@@ -32,21 +33,17 @@ import java.io.Serializable;
 public class AlarmBroadcast extends BroadcastReceiver {
     private static final String CHANNEL_ID = "MyNotificationChannel";
     private static final int NOTIFICATION_ID = 1;
-
     private OnNotifyRemainderListener listener;
-
-
     @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     public void onReceive(Context context, Intent intent) {
 
-
         String title = intent.getStringExtra(RemainderDetailsActivity.REMAINDER_TITLE);
         int ID = intent.getIntExtra(RemainderDetailsActivity.REMAINDER_ID,-1);
-        if (RemainderDetailsActivity.onNotifyRemainderListener != null){
-            listener = RemainderDetailsActivity.onNotifyRemainderListener;
-        }
-        Toast.makeText(context, "IDdddd: " +ID, Toast.LENGTH_SHORT).show();
+        UtilApplication app = UtilApplication.getInstance();
+        RemainderDetailsActivity activity = app.getActivity();
+        listener = (OnNotifyRemainderListener) activity;
+
 
         NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notification;
@@ -59,7 +56,7 @@ public class AlarmBroadcast extends BroadcastReceiver {
         Notification.InboxStyle inboxStyle = new Notification.InboxStyle()
                 .addLine(title)
                 .setBigContentTitle(title);
-        Toast.makeText(context, "step 1", Toast.LENGTH_SHORT).show();
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             notification = new Notification.Builder(context)
@@ -71,7 +68,7 @@ public class AlarmBroadcast extends BroadcastReceiver {
                     .setContentIntent(pendingIntent)
                     .build();
             nm.createNotificationChannel(new NotificationChannel(CHANNEL_ID,"new channel",NotificationManager.IMPORTANCE_HIGH));
-            Toast.makeText(context, "step 2", Toast.LENGTH_SHORT).show();
+
         }
         else {
             notification = new Notification.Builder(context)
@@ -82,17 +79,11 @@ public class AlarmBroadcast extends BroadcastReceiver {
                     .setChannelId(CHANNEL_ID)
                     .setContentIntent(pendingIntent)
                     .build();
-            Toast.makeText(context, "step 3", Toast.LENGTH_SHORT).show();
+
         }
         nm.notify(NOTIFICATION_ID,notification);
-        Toast.makeText(context, "step 4", Toast.LENGTH_SHORT).show();
-        try{
-            listener.onNotifyRemainder(ID);
-            Toast.makeText(context, "step 6", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_LONG).show();
-        }
 
+        listener.onNotifyRemainder(ID);
 
     }
 }
