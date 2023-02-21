5485105b29f50ab740845178934e12a5f2cdb480
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-21 21:04:35 +0530

    Added a listener

--- Tasktracker/app/src/main/java/com/example/tasktracker/OnAddNoteRemainderListener.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/OnAddNoteRemainderListener.java
@@ -1,2 +1,12 @@
-package com.example.tasktracker;public interface OnAddNoteRemainderListener {
+package com.example.tasktracker;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.tasktracker.entities.Remainder;
+
+public interface OnAddNoteRemainderListener {
+    int onAddNoteRemainderListener(Context context);
+    LiveData<Remainder> onGettingRemainderByID(int ID);
 }
