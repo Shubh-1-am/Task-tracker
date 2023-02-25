fa03f1c1cd144e51102458bfaa7b608f1c0a5740
Shubh-1-am <aspiretoinspire2004@gmail.com>
2023-02-25 22:59:52 +0530

    Updated manifest file

--- Tasktracker/app/src/main/java/com/example/tasktracker/SplashActivity.java
+++ Tasktracker/app/src/main/java/com/example/tasktracker/SplashActivity.java
@@ -2,13 +2,32 @@ package com.example.tasktracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(i);
+                    finish ();
+                }
+            }
+        };
+        thread.start();
     }
 }
